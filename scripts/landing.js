7875dfee1cdc8b45fae37c39baf298c637fd19f8
“Rob” <rgrabowski1@gmail.com>
2017-02-08 17:27:21 -0500

    Changes for iPad landscape

--- scripts/landing.js
+++ scripts/landing.js
@@ -139,7 +139,7 @@ var animateBox = function(whichOption) {
 $(window).load(function() {
     // Automatically animate the points on a tall screen where scrolling can't trigger the animation
     
- if ($(window).width() > 900) {
+ if ($(window).width() > 900 AND $(window).height() > $(window).width()) {
          
      
     
